176da123799410a2ceba06f0861094e878c8625a
mattej5 <65405308+mattej5@users.noreply.github.com>
2024-12-02 23:06:34 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-let app = express();
+const app = express();
 let path = require("path");
 const port = process.env.port || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -15,8 +18,8 @@ const knex = require("knex")({
     connection: { // Database connection details.
         host: process.env.RDS_HOSTNAME || "localhost", // Host where the PostgreSQL server is running.
         user: process.env.RDS_USERNAME || "postgres", // PostgreSQL user with access to the database.
-        password: process.env.RDS_USERNAME || "", // Password for the PostgreSQL user.
-        database: process.env.RDS_DB_NAME || "tsp_db", // Database name.
+        password: process.env.RDS_PASSWORD || "supersecretpassword", // Password for the PostgreSQL user.
+        database: process.env.RDS_DB_NAME || "ebdb", // Database name.
         port: process.env.RDS_PORT || 5432, // Default port for PostgreSQL.
         ssl: process.env.DB_SSL ? {rejectUnauthorized : false} : false
     }
@@ -24,12 +27,7 @@ const knex = require("knex")({
 
 // Home route
 app.get("/", (req, res) => {
-    knex.select().from("student").then(student => {
-        res.render("index", { student: student });
-    }).catch(err => {
-        console.error("Database Error:", err);
-        res.status(500).json({ error: "Failed to fetch students." });
-    });
+    res.render("index");
 });
 
 // Start the server
