0ae13b21baa1f064dac394e7caae62d804259e8b
phatch21 <153117295+phatch21@users.noreply.github.com>
2024-12-04 11:12:38 -0700

    Update 00_get_certificate.sh

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-# Place in .platform/hooks/postdeploy directory
-sudo certbot -n --expand \
-  -d turtle4.us-east-1.elasticbeanstalk.com \
-  -d tsp.theaianalyst.net \
-  --nginx --agree-tos --email phatch21@byu.edu
+sudo certbot -n -d tsp.theaianalyst.net --nginx --agree-tos --email phatch21@byu.edu
